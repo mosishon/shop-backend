978d678c8add1a6957ea6ba18cb3958f77b379ac
mosTafa <mostafa1382ma@gmail.com>
2024-04-04 06:40:06 +0330

    change login method

--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
-from dependencies import verify_user
+from dependencies import verify_user,verify_user_bypass_login_page
 from database.models import User
 from database.connection import engine
 from typing import Annotated
@@ -8,23 +9,23 @@ from api.models import UserLoginCredential
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from auth import verify_password_hash,create_jwt_token
-router = APIRouter(prefix="/user",tags=['user'],dependencies=[Depends(verify_user)])
+router = APIRouter(prefix="/user",tags=['user'],dependencies=[Depends(verify_user_bypass_login_page)])
 
 
 
 
 
-UserDepend = Annotated[User,Depends(verify_user)]
+UserDepend = Annotated[verify_user,Depends()]
 @router.get("/me",response_class=JSONResponse)
 async def get_me(user:UserDepend):
     return user
     
 @router.post("/login",response_class=JSONResponse)
-async def user_login(login_credential:UserLoginCredential):
+async def user_login(login_credential:Annotated[OAuth2PasswordRequestForm,Depends()]):
     with Session(engine) as ses,ses.begin():
         user = ses.scalar(select(User).where(User.username == login_credential.username))
         if user and verify_password_hash(login_credential.password,user.password):
             token = create_jwt_token({"uid":user.id})
-            return {"token":token}
-        
+            return {"access_token":token,"token_type":"bearer"}
+        raise HTTPException(401,"Unauthorized")
 

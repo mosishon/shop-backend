8ab0cc1845e2ed238af1490a9e67f18fd2f4e00b
mosTafa <mostafa1382ma@gmail.com>
2024-04-04 06:44:25 +0330

    create static_files_path if not exists and add file router

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,APIRouter,Request
+from fastapi import FastAPI
 from config import settings
 import os
 from api.endpoints.user import router as user_router
@@ -6,13 +6,17 @@ from api.endpoints.order import router as order_router
 from api.endpoints.product import router as product_router
 from api.endpoints.comment import router as comment_router
 from api.endpoints.category import router as category_router
+from api.endpoints.file import router as file_router
 
 
 
+if not os.path.exists(settings.static_files_path):
+    os.makedirs(settings.static_files_path)
 
 os.environ['TZ'] = settings.timezone
 
-
+if settings.env=="debug":
+    print("RUNNING DEBUG")
 
 application = FastAPI(debug=settings.env=="debug")
 
@@ -21,4 +25,5 @@ application.include_router(order_router,prefix='/api/v1')
 application.include_router(product_router,prefix='/api/v1')
 application.include_router(comment_router,prefix='/api/v1')
 application.include_router(category_router,prefix='/api/v1')
+application.include_router(file_router,prefix='/api/v1')
     

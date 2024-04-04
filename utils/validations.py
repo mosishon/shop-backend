2a96f00157b41fe98e7cf74e9d8486d344081493
mosTafa <mostafa1382ma@gmail.com>
2024-04-04 06:45:56 +0330

    [+] Add function 'is_image'|

---
+++ utils/validations.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from typing import BinaryIO
+
+
+def is_image(file: BinaryIO):
+    try:
+        # Open the file as an image
+        with Image.open(file) as img:
+            # Check if it's a valid image
+            img.verify()
+            return True
+    except Exception as e:
+        # If an error occurs or the file is not a valid image
+        return False

d1ad50f1b57453afe223d40dc36f55d80448e2cd
mosTafa <mostafa1382ma@gmail.com>
2024-04-05 08:16:48 +0330

    Read uri from .env

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,7 @@
 from sqlalchemy.engine import create_engine
+from config import settings
 
-engine = create_engine("sqlite:///db.sqlite3",echo=True)
+engine = create_engine(settings.database_uri,echo=settings.env=="debug")
 
 
 
